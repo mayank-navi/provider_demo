c1f7c85efd213ea7c3ce39fc183f3b386ac00379
mayank-navi <mayank.sheoran@navi.com>
2022-07-07 15:15:00 +0530

    working except get api

--- src/main/java/com/provider_info_demo_project/provider_info/ProviderInfoApplication.java
+++ src/main/java/com/provider_info_demo_project/provider_info/ProviderInfoApplication.java
@@ -7,7 +7,9 @@ import com.provider_info_demo_project.provider_info.provider.ProviderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,6 +27,12 @@ public class ProviderInfoApplication {
 		return providerRepository.findAll();
 	};
 
+	@PostMapping("getProvider")
+	public Provider getProvider(@RequestParam String provider, @RequestParam String flowName) {
+		ProviderPKid id = new ProviderPKid(provider, flowName);
+		return providerRepository.findById(id).orElse(null);
+	};
+
 	@PostMapping("addProvider")
 	public Provider addProvider(@RequestParam String provider, @RequestParam String flowName, @RequestBody DownTime downTimeInfo) {
 		DownTime downTime = new DownTime(downTimeInfo.downTimeStart, downTimeInfo.downTimeEnd);
@@ -33,14 +41,17 @@ public class ProviderInfoApplication {
 	};
 
 	@PostMapping("updateProvider")
-	public Provider updateProvider(Provider provider) {
-		return providerRepository.save(provider);
+	public Provider updateProvider(@RequestParam String provider, @RequestParam String flowName, @RequestBody DownTime downTimeInfo) {
+		DownTime downTime = new DownTime(downTimeInfo.downTimeStart, downTimeInfo.downTimeEnd);
+		Provider newProvider = new Provider(provider, flowName, downTime.downTimeStart, downTime.downTimeEnd);
+		return providerRepository.save(newProvider);
 	};
 
 	@PostMapping("deleteProvider")
-	public Provider deleteProvider(@RequestParam String provider, @RequestParam String flowName) {
+	public void deleteProvider(@RequestParam String provider, @RequestParam String flowName) {
 		ProviderPKid id = new ProviderPKid(provider, flowName);
-		return providerRepository.deleteById(id);
+		providerRepository.deleteById(id);
+		return;
 	};
 
 
